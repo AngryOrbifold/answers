1209e207e12e31be4f70e265ddb3a61d9c8e9dc3
AngryOrbifold <angryorbifold@gmail.com>
2025-05-28 17:33:33 +0200

    Update panel.js

--- panel.js
+++ panel.js
@@ -297,11 +297,13 @@ async function sendAnswer() {
       // Threshold canvas to black & white
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
       const data = imageData.data;
+      /*
       for (let i = 0; i < data.length; i += 4) {
         const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
         const value = avg > 127 ? 255 : 0;
         data[i] = data[i + 1] = data[i + 2] = value;
       }
+      */
       ctx.putImageData(imageData, 0, 0);
 
       // Get base64 image string (without data:image/... prefix)
